08b26101dd0ca634d4eea9a83aad56e0c501bee1
amandaresende <amandacamposresende@gmail.com>
2022-08-02 17:59:19 -0300

    feat: mudanças no about

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,12 +1,21 @@
 import './About.css'
 import React from 'react'
 
+
+
 const About = props => (
     <div className="about">
         <div className="text-about">
-        <span className="first-text-about">Sobre</span>
-        <span className="second-text-about">--Quem eu sou--</span>
+            <span className="first-text-about">Sobre</span>
+            <span className="second-text-about">--Quem eu sou--</span>
         </div>
+
+        <section>
+        <div className="background"></div>
+        <div className="background"></div>
+        <div className="background"></div>
+        </section>
+
 
     </div>
 
