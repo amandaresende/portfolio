909bbe9f2fdbef88be9de7a04261891427fe2415
amandaresende <amandacamposresende@gmail.com>
2022-08-04 15:53:52 -0300

    feat: Ajustes na 'skills' e no 'contato'

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -7,12 +7,25 @@ const Contact = props => (
         <div className="text-principal"> 
         <span className="first-text-contact">Vamos conversar?</span>
         </div>
-
             <div class="text-about">
-                <div class="second-text-contact"></div>
-                <p>Acompanhe o que estou desenvolvendo no meu Github ou entre em contato comigo.
-                </p>
-
+                <div class="text-about-name">
+                            <div class="head">Nome</div>
+                            <div class="sub-title">Amanda Resende</div>
+                        </div>
+                   </div>
+                   <div class="row">
+                    <div class="info">
+                        <div class="head">Endereço</div>
+                        <div class="sub-title">Juiz de Fora, MG</div>
+                    </div>
+               </div>
+            <div class="row">
+                <div class="info">
+                    <div class="head">E-mail</div>
+                    <div class="sub-title">amandacamposresende@gmail.com
+                    </div>
+                </div>
+           
             </div>
 
 
---
+++ src/views/Skills/Skills.jsx
@@ -0,0 +1,36 @@
+import './Skills.css'
+import React from 'react'
+
+const Skills = props => (
+    <div className="skills">
+        <div className="text-principal">
+            <span className="first-text-skill">Minhas Habilidades</span>
+
+        </div>
+
+        <div className="icon-skills">
+            <span className="background"></span>
+            <span className="background"></span>
+            <span className="background"></span>
+            <span className="background"></span>
+
+            <div className="text-skill">
+                <div className="text-skills-skills">
+                    <p>
+                        No momento estou me aplicando ao desenvolvimento de projetos que envolvem as linguagens de programação
+                        Front-End como o JavaScript e React. Todos os projetos têm sido compartilhados no meu <a href="https://github.com/amandaresende"class="social-network"> <b>GitHub</b> </a> 
+                        <a href="https://github.com/amandaresende"/>
+                    </p>
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+
+    </div>
+)
+
+export default Skills
