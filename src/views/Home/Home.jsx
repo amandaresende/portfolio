60cb0822e13e794074314774114d96d46aaab6a4
amandaresende <amandacamposresende@gmail.com>
2022-08-05 17:43:48 -0300

    feat: Ajustes de cor no 'Home' e 'Menu'

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -7,16 +7,21 @@ import ReactTypingEffect from 'react-typing-effect';
 const Home = props => (
     <div className="home">
         <div className="text-home">
-            <span className="first-text">Olá, meu nome é </span>
-            <span className="second-text">Amanda </span>
-            <span className="">eu sou </span>
-            <ReactTypingEffect 
-                className="text-typing" 
-                text={["Desenvolvedora", "Front-End"]} 
-            />
+            <span className="first-text">Olá, meu nome é </span> <br />
+            <span className="second-text">Amanda</span> <br/>
+            <span className="third-text">eu sou </span>
+        </div>
+        <div className="text-home-second">
             
         </div>
-        <Girl className="img-home" />
+
+        {/* <ReactTypingEffect 
+                className="text-typing" 
+                text={["Desenvolvedora", "Front-End"]} 
+            />  */}
+
+
+        {/* <Girl className="img-home" /> */}
     </div>
 
 
