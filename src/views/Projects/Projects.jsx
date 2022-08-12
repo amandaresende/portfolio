ec7c704dff0450280a2ba0610939d5b11a4c0a65
amandaresende <amandacamposresende@gmail.com>
2022-08-11 22:37:06 -0300

    fix: Ajustes no 'projects' e em 'skills'

--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -1,13 +1,31 @@
 import './Projects.css'
 import React from 'react'
+import  { useState } from 'react';
+
+
 
 const Projects = props => (
+        
+ 
+
     <div className="titulo-projects">
         <h1> Projetos </h1>
         <div className="content-projects">
+
+            <div className="algo">
+            <span className="background"></span>
+            <span className="background"></span>
+            <span className="background"></span>
        
+
+
+ 
         </div>
-    </div>
+
+        </div>
+
+        </div>
+    
 )
 
 export default Projects
